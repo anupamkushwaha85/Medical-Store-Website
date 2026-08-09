ca51dbda5c6bde521e93122bca3cf4b4d5eed55c
anupamkushwaha85 <149708150+anupamkushwaha85@users.noreply.github.com>
2026-08-09 16:54:00 +0530

    feat: add vercel configuration for routing and update base path in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/Medical-Store-Website/',
+    base: '/',
     plugins: [react()],
     server: {
         host: '0.0.0.0',
