6a59de040c5d216fadec918fca7445877980428c
anupamkushwaha85 <149708150+anupamkushwaha85@users.noreply.github.com>
2026-08-14 18:36:20 +0530

    feat: added rate limiting

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const globalLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window`
+    max: 500, // Limit each IP to 500 requests per `window`
     message: { error: 'Too many requests from this IP, please try again after 15 minutes' },
     standardHeaders: true,
     legacyHeaders: false,
